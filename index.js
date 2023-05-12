fcfbe2f7393fc701431f6edad1dcad0c057abaf5
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-05-12 08:47:08 +0300

    Done

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const randomWords=document.querySelector(".head")
 const button=document.querySelector(".inputs")
 const result=document.querySelector(".result")
 const newButton=document.querySelector(".newGame")
+const head=document.querySelector("#head")
+const body=document.querySelector("#body")
+const leftarm=document.querySelector("#leftarm")
+const rightarm=document.querySelector("#rightarm")
+const leftfood=document.querySelector("#leftfood")
+const rightfood=document.querySelector("#rightfood")
+const newGameButton=document.getElementById("newGameButton")
+
 
 let game=["MonsterInc","Pokemon","OnePiece"]
 
@@ -40,6 +48,9 @@ const alphabet = [
     button.appendChild(btn)
     btn.textContent=item
     btn.value=item
+    btn.style.width="300px"
+    btn.style.width="200px"
+    btn.style.margin="10px"
     btn.setAttribute("id",`${item}`)
   })
  
@@ -54,14 +65,88 @@ newArr.map((item,index)=>{
     span.textContent=item
     span.classList=index
     span.style.padding="20px"
+    
 })
-
+console.log(newArr)
 const inputs=document.querySelectorAll("button")  
 
+let guesses = 0 
+let maxGuesses = 6 
+
+inputs.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const letter = e.target.value.toUpperCase()
+    const spans = document.querySelectorAll("span")
+    let found = false
+
+    newArr.forEach((char, index) => {
+      if (char.toUpperCase() === letter) {
+        spans[index].textContent = letter
+        found = true
+      }
+   
+    });
+
+    if (!found) {
+      guesses++
+      hangMan(guesses)
+    }
+
+    if (guesses >= maxGuesses) {
+      result.textContent = `Game over! The word was ${newWord}.`
+      button.style.display = "none"
+      newButton.style.display = "block"
+      newGameButton.style.display=""
+    }
+    let isComplete = true;
+    for (let i = 0; i < spans.length; i++) {
+      if (spans[i].textContent === "___") {
+        isComplete = false
+        break
+      }
+    }
+
+    if (isComplete) {
+      result.textContent = `Congratulations! You guessed the word ${newWord}.`
+      button.style.display = "none"
+      newButton.style.display = "block"
+      newGameButton.style.display=""
+    }
+  
+   
+  })
+})
+
+function hangMan(guesses) {
+  switch (guesses) {
+    case 1:
+      head.style.visibility=""
+      break
+    case 2:
+      body.style.visibility=""
+      break
+    case 3:
+      leftarm.style.visibility=""
+      break
+    case 4:
+      rightarm.style.visibility=""
+      break
+    case 5:
+      leftfood.style.visibility=""
+      break
+    case 6:
+      rightfood.style.visibility=""
+      break
+  }
+}
+
+function reloadPage() {
+  location.reload()
+}
+newGameButton.addEventListener("click",()=>{
+  reloadPage()
+})
 
 
 
 
-
-
-//  randomWords.innerHTML=newWord
